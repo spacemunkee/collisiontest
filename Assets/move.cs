65d6ea85dee22fac76b4470c454a540d5ac984ea
Doug Linley <spacemunkee.codes@gmail.com>
2017-03-11 15:10:08 -0500

    Initial check-in

---
+++ Assets/move.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+
+	public float speed = 5f;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += new Vector3(0, Input.GetAxis("Vertical") * speed * Time.deltaTime, 0);
+	}
+}
